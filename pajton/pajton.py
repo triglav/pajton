9667ddd84d89a1d2813a690e05f238467a5dd699
Matej Svec <matej@msvec.sk>
2018-07-14 11:03:18 +0200

    Add apple, snake growing, end game

--- pajton/pajton.py
+++ pajton/pajton.py
@@ -1,4 +1,4 @@
-import sys, pygame, itertools
+import sys, pygame, itertools, random
 from enum import Enum, unique, auto
 from collections import deque, namedtuple
 
@@ -14,6 +14,8 @@ assert 1000 % kTicksPerSecond == 0, "(1000 / kTicksPerSecond) should result in z
 
 kSkipTicks = 1000 / kTicksPerSecond
 kMaxFrameSkip = kTicksPerSecond / 5
+
+random.seed()
 
 pygame.init()
 screen = pygame.display.set_mode([kWidth, kHeight], pygame.HWSURFACE | pygame.DOUBLEBUF)
@@ -31,16 +33,19 @@ Point = namedtuple("Point", "x y")
 class Snake:
     def __init__(self, x = int(kWorldWidth/2), y = int(kWorldHeight/2), direction = Direction.DOWN):
         self.direction = direction
-        self.speed = 1
+        self.speed = 2
         self.i = 0
-        assert(x-1 >= 0)
-        self.__body = deque([Point(x-1,y), Point(x,y)])
+        self.parts = deque([Point(x,y)])
+        self.Grow()
 
     def head(self):
-        return self.__body[-1]
+        return self.parts[-1]
 
     def body(self):
-        return itertools.islice(self.__body, len(self.__body)-1)
+        return itertools.islice(self.parts, len(self.parts)-1)
+
+    def Grow(self):
+        self.__do_grow = True
 
     def Update(self):
         self.i += 1
@@ -91,27 +96,53 @@ class Snake:
             if head.x < 0:
                 head = Point(kWorldWidth - 1, head.y)
 
-        self.__body.append(head)
-        self.__body.popleft()
+        self.parts.append(head)
+        if self.__do_grow:
+            self.__do_grow = False
+        else:
+            self.parts.popleft()
         
 
 class Game:
+    @unique
+    class State(Enum):
+        RUNNING = auto()
+        PAUSE = auto()
+        END_GAME = auto()
+        QUIT = auto()
+
     def __init__(self):
-        self.running = True
+        self.state = Game.State.RUNNING
         self.snejk = Snake()
+        self.SpawnApple()
+
+    def SpawnApple(self):
+        while 1:
+            self.apple = Point(
+                random.randint(0, kWorldWidth - 1),
+                random.randint(0, kWorldHeight - 1))
+            if self.apple not in self.snejk.parts:
+                break
 
     def Update(self):
-        self.snejk.Update()
+        if self.state == Game.State.RUNNING:
+            self.snejk.Update()
+            if self.snejk.head() in self.snejk.body():
+                print("BANG - END OF THE GAME")
+                self.state = Game.State.END_GAME
+            if self.snejk.head() == self.apple:
+                print("HAM HAM HAM")
+                self.snejk.Grow()
+                self.SpawnApple()
         
     def PumpEvents(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
+                self.state = Game.State.QUIT
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    self.running = False
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
+                    self.state = Game.State.QUIT
+                elif event.key == pygame.K_RIGHT:
                     self.snejk.TurnRight()
                 elif event.key == pygame.K_LEFT:
                     self.snejk.TurnLeft()
@@ -119,15 +150,18 @@ class Game:
 gejm = Game()
 
 def Draw(surface, g):
-    def DrawSnakePart(surface, color, pos):
+    def DrawTile(surface, color, pos):
         pygame.draw.rect(surface, color, (pos.x*kTileSize, pos.y*kTileSize, kTileSize, kTileSize))
-
+    # Draws apple
+    DrawTile(surface, (155, 155, 0), g.apple)
+    # Draws snake
     for part in g.snejk.body():
-        DrawSnakePart(surface, (0, 155, 0), part)
-    DrawSnakePart(surface, (255, 0, 0), g.snejk.head())
+        DrawTile(surface, (0, 155, 0), part)
+    DrawTile(surface, (255, 0, 0), g.snejk.head())
+    
 
 next_tick = pygame.time.get_ticks()
-while gejm.running:
+while gejm.state != Game.State.QUIT:
     loops = 0
     while pygame.time.get_ticks() > next_tick and loops < kMaxFrameSkip:
         gejm.PumpEvents()
